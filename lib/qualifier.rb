7b1ff10d8f465dc3a85558009582976edbbd869c
Russell Vaughan <russell@gosquared.com>
2016-11-23 09:32:22 +0000

    add error handling for html only

--- lib/qualifier.rb
+++ lib/qualifier.rb
@@ -2,56 +2,55 @@ require './lib/driver'
 
 class Qualifier
 
-attr_reader :qualification
+  attr_reader :qualification
 
-USER_SYSTEM = %w(sign\ in log\ in LOGIN log_in Login sign\ up register Sign\ In Sign\ Up Log\ in Sign\ in Log\ In)
+  USER_SYSTEM = %w(sign\ in log\ in LOGIN log_in Login sign\ up register Sign\ In Sign\ Up Log\ in Sign\ in Log\ In)
 
-def initialize(website)
+  def initialize(website)
     @website = website
     @qualification = {}
-end
-
-def qualify
-@user_sites = Driver.new(@website).find_sites
-num = 0
-p @user_sites
-while @user_sites == []  && num < 10
-p "#{num += 1}"
-@user_sites = Driver.new(@website).find_sites
-p @user_sites
-end
-if num == 10 && @user_sites == []
-@qualification['site']='no sites present'
-else
-qualify_user_sites
-end
-end
-
-def qualify_user_sites
-@user_sites.length.times do |num|
-@site = instance_variable_set("@site_#{num}", Hash.new)
-begin
-  response = HTTParty.get(@user_sites[num])
-rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Errno::ECONNREFUSED,
-      Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, Errno::ETIMEDOUT, URI::InvalidURIError => error
-  @site['url']=@user_sites[num]
-  puts "Site does not load: #{error}"
-  @site['site_status']=error
-  @qualification["site_#{num}"]= @site
-   puts @qualification
-end
-   if response
-    @site['url']=@user_sites[num]
-    @site['site_status']=response.message
-    if @site['site_status'] == "OK"
-    qualify_page(@user_sites[num])
-    @qualification["site_#{@num}"]= @site
   end
-   puts @qualification
-   end
 
-end
-@qualification
+  def qualify
+    @user_sites = Driver.new(@website).find_sites
+    num = 0
+    p @user_sites
+    while @user_sites == []  && num < 10
+      p "#{num += 1}"
+      @user_sites = Driver.new(@website).find_sites
+      p @user_sites
+    end
+    if num == 10 && @user_sites == []
+      @qualification['site']='no sites present'
+    else
+      qualify_user_sites
+    end
+  end
+
+  def qualify_user_sites
+    @user_sites.length.times do |num|
+      @site = instance_variable_set("@site_#{num}", Hash.new)
+      begin
+        response = HTTParty.get(@user_sites[num])
+      rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Errno::ECONNREFUSED,
+        Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError, Errno::ETIMEDOUT, URI::InvalidURIError => error
+        @site['url']=@user_sites[num]
+        puts "Site does not load: #{error}"
+        @site['site_status']=error
+        @qualification["site_#{num}"]= @site
+        puts @qualification
+      end
+      puts @user_sites[num]
+      if response
+        @site['url']=@user_sites[num]
+        @site['site_status']=response.message
+       qualify_page(@user_sites[num]) if response.headers['content-type'].include?('html')
+        @qualification["site_#{@num}"]= @site
+        puts @qualification
+    end
+
+  end
+  @qualification
 end
 
 def qualify_page(website)
@@ -59,11 +58,11 @@ def qualify_page(website)
   @goodfit = nil
   @badfit = nil
   page = HTTParty.get(website)
-  @page = Nokogiri::HTML(page)
-  login
-  built_with
-  analytics_scripts
-  fit
+    @page = Nokogiri::HTML(page)
+    login
+    built_with
+    analytics_scripts
+    fit
 end
 
 def header
@@ -81,12 +80,12 @@ end
 def login
   USER_SYSTEM.each do |x|
     x = (@page.search "[text()*='"+x+"']")
-   if x.to_s.strip.length == 0
-   else
-   @usersystem = x
-   @goodfit = true
-  end
-end
+    if x.to_s.strip.length == 0
+    else
+     @usersystem = x
+     @goodfit = true
+   end
+ end
  if @usersystem
   @site['user_system']=true
 else
@@ -115,36 +114,36 @@ end
 def built_with
   string = header.to_s.downcase
   if  @usersystem && string.include?('wp-content')
-     @site['built_with']='Wordpress'
-  elsif string.include?('wp-content')
-    @site['built_with']='Wordpress'
-    @badfit = true
-  end
-  if string.include?('wix')
-    @site['built_with']='Wix'
-    @badfit = true
-  end
-  if string.include?('squarespace')
-    @site['built_with']='SquareSpace'
-    @badfit = true
-  end
-  if string.include?('rapidweaver')
-    @site['built_with']='Rapidweaver'
-    @badfit = true
-  end
-    if string.include?('tumblr')
-    @site['built_with']='tumblr'
-    @badfit = true
-  end
+   @site['built_with']='Wordpress'
+ elsif string.include?('wp-content')
+  @site['built_with']='Wordpress'
+  @badfit = true
+end
+if string.include?('wix')
+  @site['built_with']='Wix'
+  @badfit = true
+end
+if string.include?('squarespace')
+  @site['built_with']='SquareSpace'
+  @badfit = true
+end
+if string.include?('rapidweaver')
+  @site['built_with']='Rapidweaver'
+  @badfit = true
+end
+if string.include?('tumblr')
+  @site['built_with']='tumblr'
+  @badfit = true
+end
 end
 
-  def fit
-    if  @site['user_system']==true && @site['built_with'].nil?
+def fit
+  if  @site['user_system']==true && @site['built_with'].nil?
     @site['fit']='People, Analytics and Chat'
-    elsif
+  elsif
     @site['user_system']== true && @site['built_with'] == 'Wordpress'
     @site['fit']='People, Analytics and Chat'
-    else
+  else
     @site['fit']='Analytics & Chat only'
   end
 end
